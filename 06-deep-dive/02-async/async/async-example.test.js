17aa34bd72e3818123daff1881b913668d05861b
pixaily <danield.georgiev@gmail.com>
2023-11-29 17:02:42 +0200

    Javascript Testing - Deep Dive - Test async code with async/await

--- 06-deep-dive/02-async/async/async-example.test.js
+++ 06-deep-dive/02-async/async/async-example.test.js
@@ -23,3 +23,11 @@ it('should generate a token value', () => {
 
   return expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined();
 })
+
+it('should generate a token value', async () => {
+  const testUserEmail = 'test@test.com';
+
+  const token = await generateTokenPromise(testUserEmail);
+
+  expect(token).toBeDefined();
+})
